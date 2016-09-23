5b3bb5a093743a87592005a3c4a36afed8b9be4d
ay3 <draft@perl.pink>
2016-09-23 21:49:48 +0900

    [add]comment追加

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -10,6 +10,8 @@ class SpotsController < ApplicationController
   # GET /spots/1
   # GET /spots/1.json
   def show
+    @comments = @spot.comments.all
+    @comment = @spot.comments.build
   end
 
   # GET /spots/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923112934) do
+ActiveRecord::Schema.define(version: 20160923123716) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "spot_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "spots", force: :cascade do |t|
     t.string   "name"
