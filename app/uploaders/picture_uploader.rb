2dbb97271b637e509e68d2d39ae24e4c6509fc06
ay3 <draft@perl.pink>
2016-09-24 21:53:20 +0900

    [mod]picture設定変更

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,9 +5,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  version :thumb do
-    process :resize_to_fill => [320, 240]
-  end
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -34,21 +31,24 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  #リサイズする場合の上限
+  #process :resize_to_limit => [320, 240]
+
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+     process :resize_to_fit => [200, 150]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+     "#{Time.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename
+  end
 
 end
