ee9dc8aff8643285ffd44ba5e107ab463151054e
ay3 <draft@perl.pink>
2016-09-29 22:34:26 +0900

    [mod]category,prefectureをプルダウン選択形式に変更

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -14,17 +14,12 @@ class Spot < ApplicationRecord
   presence: { message: "何か入力して下さい。" },
   length: { in: 2..30, message: "2〜30文字で入力して下さい。" }
 
-  validates :prefectures, length: { maximum: 3 }
   validates :postcode,    length: { maximum: 7 }
   validates :address,     length: { maximum: 30 }
   validates :mapcode,     length: { maximum: 15 }
   #validates :latitude,    numericality: true
   #validates :longitude,   numericality: true
   validates :access,      length: { maximum: 100 }
-
-  validates :category,
-  presence: { message: "何か入力して下さい。" },
-  length: { in: 2..30, message: "2〜30文字で入力して下さい。" }
 
   validates :url,         length: { maximum: 100 }
   validates :memo,        length: { maximum: 500 }
